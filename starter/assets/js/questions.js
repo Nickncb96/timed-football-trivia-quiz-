235d83808d5dd1dd626e29f0c073da69bac78404
nickncb96 <nicholasncb12@hotmail.com>
2024-01-13 03:31:12 +0000

    addig the questions for the football trivia quiz

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,58 @@
+// questions.js
+const footballQuestions = [
+    {
+        question: "Which country won the FIFA World Cup in 2018?",
+        choices: ["Germany", "Brazil", "France", "Argentina"],
+        correctAnswer: "France",
+    },
+    {
+        question: "Who is the all-time top scorer in the UEFA Champions League?",
+        choices: ["Lionel Messi", "Cristiano Ronaldo", "Robert Lewandowski", "Raul"],
+        correctAnswer: "Cristiano Ronaldo",
+    },
+    {
+        question: "Which team has won the most Premier League titles?",
+        choices: ["Manchester United", "Liverpool", "Arsenal", "Chelsea"],
+        correctAnswer: "Manchester United",
+    },
+    {
+        question: "In which year did the first FIFA World Cup take place?",
+        choices: ["1930", "1950", "1970", "1990"],
+        correctAnswer: "1930",
+    },
+    {
+        question: "Who is known as 'The Egyptian King' in football?",
+        choices: ["Mohamed Salah", "Sadio Mane", "Roberto Firmino", "Virgil van Dijk"],
+        correctAnswer: "Mohamed Salah",
+    },
+    {
+        question: "Which country has won the most Copa America titles?",
+        choices
+
+        : ["Brazil", "Argentina", "Uruguay", "Chile"],
+        correctAnswer: "Uruguay",
+    },
+    {
+        question: "Who won the Ballon d'Or in 2020?",
+        choices: ["Lionel Messi", "Robert Lewandowski", "Cristiano Ronaldo", "Virgil van Dijk"],
+        correctAnswer: "No award (canceled due to COVID-19)",
+    },
+    {
+        question: "Which club has the nickname 'The Red Devils'?",
+        choices: ["Manchester United", "Liverpool", "AC Milan", "Barcelona"],
+        correctAnswer: "Manchester United",
+    },
+    {
+        question: "Who is the manager of the German national football team as of 2022?",
+        choices: ["Joachim Low", "Jurgen Klopp", "Thomas Tuchel", "Pep Guardiola"],
+        correctAnswer: "Hans-Dieter Flick",
+    },
+    {
+        question: "Which stadium is known as 'The Theatre of Dreams'?",
+        choices: ["Old Trafford", "Anfield", "Camp Nou", "Santiago Bernabeu"],
+        correctAnswer: "Old Trafford",
+    },
+];
+
+export default footballQuestions;
+
